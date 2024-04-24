10dd8e1e6a61621dcd7149a8252e473270dbc01b
larian3 <larianelima22@gmail.com>
2024-04-24 15:03:30 -0300

    responsividade na lista de produtos

--- src/app/components/Product.tsx
+++ src/app/components/Product.tsx
@@ -1,5 +1,6 @@
 import { ProductType } from "@/types/ProductType";
 import { ShareIcon, ArrowsRightLeftIcon, HeartIcon } from "@heroicons/react/24/outline";
+import ProductImage from "./ProductImage";
 
 type ProductProps = {
   product: ProductType
@@ -8,28 +9,34 @@ type ProductProps = {
 export default function Product({product}:ProductProps) {
 
   return (
-<div className="flex flex-col shadow-md h-96 bg-[#F4F5F7] font-custom relative group">
-  <div className="relative max-h-[72px] flex-1">{product.image}</div>
-  <div className="justify-between font-bold my-3 text-sm">{product.title}</div>
-  <div className="text-sm">{product.description}</div>
-  <div className="text-sm">{product.price}</div>
-  <div className="absolute inset-0 bg-gray-300 opacity-0 hover:opacity-50 transition-opacity duration-300 flex flex-col items-center justify-center group-hover:opacity-100">
-    <button className="bg-[white] text-[#B88E2F] px-6 py-2 hover:bg-[#8F6B1E] hover:text-white mb-2">
-      See Details
-    </button>
-    <div className="flex items-center text-xs">
-      <a href="#" className="text-white flex items-center hover:text-[#B88E2F] mx-1 text-xs">
-        <ShareIcon className="h-[14px] w-[14px] mr-1" /> Share
-      </a>
-      <a href="#" className="text-white flex items-center hover:text-[#B88E2F] mx-1 text-xs">
-        <ArrowsRightLeftIcon className="h-[14px] w-[14px] mr-1" /> Compare
-      </a>
-      <a href="#" className="text-white flex items-center hover:text-[#B88E2F] mx-1 text-xs">
-        <HeartIcon className="h-[14px] w-[14px] mr-1" /> Like
-      </a>
+<div className="flex flex-wrap justify-center">
+  <div className="flex flex-col shadow-md w-full sm:w-full md:w-full lg:w-[285px] xl:w-[285px] h-auto sm:h-auto md:h-auto lg:h-[446px] xl:h-[446px] bg-[#F4F5F7] font-custom relative group mb-10 mx-4">
+    <div className="relative w-full lg:w-[285px] xl:w-[285px] h-[301px] lg:h-[301px] xl:h-[301px]">
+      <ProductImage product={product} fill />
+    </div>
+    <div className="justify-between font-bold my-3 text-sm">{product.title}</div>
+    <div className="text-sm">{product.description}</div>
+    <div className="text-sm">{product.price}</div>
+    <div className="absolute inset-0 bg-[#3A3A3A] opacity-0 hover:opacity-100 bg-opacity-80 flex flex-col items-center justify-center">
+      <button className="bg-[white] text-[#B88E2F] px-6 py-2 hover:bg-[#8F6B1E] hover:text-white mb-2">
+        See Details
+      </button>
+      <div className="flex items-center text-xs">
+        <a href="#" className="text-white flex items-center hover:text-[#B88E2F] mx-1 text-xs">
+          <ShareIcon className="h-[14px] w-[14px] mr-1" /> Share
+        </a>
+        <a href="#" className="text-white flex items-center hover:text-[#B88E2F] mx-1 text-xs">
+          <ArrowsRightLeftIcon className="h-[14px] w-[14px] mr-1" /> Compare
+        </a>
+        <a href="#" className="text-white flex items-center hover:text-[#B88E2F] mx-1 text-xs">
+          <HeartIcon className="h-[14px] w-[14px] mr-1" /> Like
+        </a>
+      </div>
     </div>
   </div>
 </div>
+
+
 
   );
   
