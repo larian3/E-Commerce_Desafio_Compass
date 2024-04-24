7e4f3ec8329e096211ae660d4af9c4222d19a3c6
larian3 <larianelima22@gmail.com>
2024-04-24 09:13:23 -0300

    pagina dos produtos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,22 @@
+import { ProductType } from "@/types/ProductType";
 
-export default function Home() {
+async function getProducts() {
+  const res = await fetch('https://fakestoreapi.com/products')
+  if(!res.ok){
+    throw new Error("falha no carregamento de dados");
+  }
+  return res.json()
+}
+
+export default async function Home() {
+  const products = await getProducts()
   return (
     <div className="max-w-7xl mx-auto pt-8 px-8 xl:px-0">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6">
-        <div className="bg-black eounded-lg shadow-lg">
-          produto 1
-        </div>
-      
+        {products.map((product:ProductType) => (
+          <div key={products.any}>(product.title)</div>
+
+        ))}
       </div>
     </div>
   );
