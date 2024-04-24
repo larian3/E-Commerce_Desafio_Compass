55ff496aecd4c6ead434ca016b19f962409045d4
larian3 <larianelima22@gmail.com>
2024-04-24 08:57:03 -0300

    home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
 
 export default function Home() {
   return (
-    <div >
-
+    <div className="max-w-7xl mx-auto pt-8 px-8 xl:px-0">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6">
+        <div className="bg-black eounded-lg shadow-lg">
+          produto 1
+        </div>
+      
+      </div>
     </div>
   );
 }
